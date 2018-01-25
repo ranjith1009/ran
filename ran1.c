21954e42a3a51eae0a96f14f72a466e9fb772600
ranjithstreak <35728127+ranjithstreak@users.noreply.github.com>
2018-01-25 02:58:45 -0800

    Create ran1.c

---
+++ ran1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int a;
+printf("Enter the number");
+scanf("%d",&a);
+if(a==0)
+printf("The Number is Zero");
+else
+if(a>0)
+printf("The number is Positive");
+else
+if(a<0)
+printf("The number is negative");
+}
